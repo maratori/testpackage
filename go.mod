3ec9ff79947d476e638397e25636b9adae4f0a2f
Marat Reymers <marat.reymers@gett.com>
2019-11-04 21:10:24 +0300

    Add testpackage analyzer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/maratori/testpackage
+
+go 1.13
+
+require golang.org/x/tools v0.0.0-20191101200257-8dbcdeb83d3f
---
+++ pkg/testpackage/testpackage_test.go
@@ -0,0 +1,26 @@
+package testpackage_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+
+	"github.com/maratori/testpackage/pkg/testpackage"
+)
+
+func TestAnalyzer_Good(t *testing.T) {
+	testdata, err := filepath.Abs("testdata/good")
+	if err != nil {
+		t.FailNow()
+	}
+	analysistest.Run(t, testdata, testpackage.Analyzer)
+}
+
+func TestAnalyzer_Bad(t *testing.T) {
+	testdata, err := filepath.Abs("testdata/bad")
+	if err != nil {
+		t.FailNow()
+	}
+	analysistest.Run(t, testdata, testpackage.Analyzer)
+}
